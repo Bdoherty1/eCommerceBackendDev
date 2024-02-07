b04a983b08da5e7c4e00acdcac81f60c163d9f88
Bdoherty1 <bdoherty988@gmail.com>
2024-02-06 22:27:45 -0600

    added the routes in product-routes.js, tag-routes.js, and category-routes.js to perform CRUD operations using Sequelize models.GET, POST, PUT, and DELETE requests for categories, products, and tags.

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,7 +11,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+// Sync Sequelize models to the database
+sequelize.sync({ force: false }).then(() => { // Set force: true to drop tables and re-create them
+  // Start the server after syncing models
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
 });
